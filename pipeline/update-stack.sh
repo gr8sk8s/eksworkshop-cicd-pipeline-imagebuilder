3c0e35b28e0c821dc1ff806792e4032e156611d9
Conrad Fernandes <ec2-user@ip-10-0-4-155.us-east-2.compute.internal>
2022-08-28 14:16:00 +0000

    changes to buildspec

--- pipeline/update-stack.sh
+++ pipeline/update-stack.sh
@@ -1,10 +1,9 @@
 # source this file 
 
 aws cloudformation update-stack \
-    --stack-name cicd-pipeline \
+    --stack-name cicd-codepipeline \
     --template-body file://$PIPELINE_FILE \
     --parameters \
-    ParameterKey=EksClusterName,ParameterValue=$CLUSTER_NAME \
     ParameterKey=GitHubUser,ParameterValue=$GitHubUser \
     ParameterKey=GitHubToken,ParameterValue=$GitHubToken \
     --capabilities CAPABILITY_IAM
